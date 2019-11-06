d805d686ff2392b1cc24b83a4042eebbad36b62d
Souvik <souvik@keus.in>
2019-11-06 11:50:06 +0530

    added length to transmit

--- IR_decoder_Mitsubishi/IR_decoder_Mitsubishi_v2.js
+++ IR_decoder_Mitsubishi/IR_decoder_Mitsubishi_v2.js
@@ -6,7 +6,7 @@ function lsb8_to_msb()
 {
     var reverse_num = 0;
     var val = 0;
-    for (var j=0; j<11; j++)
+    for (var j=1; j<12; j++)
     {
       for (var i=0; i<8; i++)
       {
@@ -24,45 +24,45 @@ function setswing(hsw, vsw)
 {
     if (hsw)
     {
-        ir_out2[5]=0x7F;
-        ir_out2[6]=0x80;
+        ir_out2[6]=0x7F;
+        ir_out2[7]=0x80;
     }
     else
     {
-        ir_out2[5]=0xFF;
-        ir_out2[6]=0x00;
+        ir_out2[6]=0xFF;
+        ir_out2[7]=0x00;
     }
     if (vsw)
     {
-        ir_out2[7]-=0x10;
-        ir_out2[8]+=0x10;
+        ir_out2[8]-=0x10;
+        ir_out2[9]+=0x10;
     }
 }
 
 function setfan(fan)
 {
-    ir_out2[7] = 0x0F;
-    ir_out2[8] = 0x00;
+    ir_out2[8] = 0x0F;
+    ir_out2[9] = 0x00;
     switch (fan) {
 
         case 1: ;
-            ir_out2[7] |= 0xF0;
-            ir_out2[8] |= 0x0;
+            ir_out2[8] |= 0xF0;
+            ir_out2[9] |= 0x0;
             break;
 
         case 2: ;
-            ir_out2[7] |= 0x70;
-            ir_out2[8] |= 0x80;
+            ir_out2[8] |= 0x70;
+            ir_out2[9] |= 0x80;
             break;
 
         case 3: ;
-            ir_out2[7] |= 0x90;
-            ir_out2[8] |= 0x60;
+            ir_out2[8] |= 0x90;
+            ir_out2[9] |= 0x60;
             break;
 
         case 4: ;
-            ir_out2[7] |= 0xB0;
-            ir_out2[8] |= 0x40;
+            ir_out2[8] |= 0xB0;
+            ir_out2[9] |= 0x40;
             break;
             
     }
@@ -71,62 +71,62 @@ function setfan(fan)
 
 function settemp(temp)
 {
-    ir_out2[9] &= 0x0F;
     ir_out2[10] &= 0x0F;
-    ir_out2[9] |= 0xE0;
-    ir_out2[10] |= 0x10;
+    ir_out2[11] &= 0x0F;
+    ir_out2[10] |= 0xE0;
+    ir_out2[11] |= 0x10;
     
     for(var i=18; i<=30; i++)
     {   
       if (i==temp){break;}
-      ir_out2[9]-=0x10;
-      ir_out2[10]+=0x10;
+      ir_out2[10]-=0x10;
+      ir_out2[11]+=0x10;
     }
 }
   
 function setmode(mode)
 {
-    ir_out2[9] &= 0xF0;
     ir_out2[10] &= 0xF0;
+    ir_out2[11] &= 0xF0;
 
     switch(mode) {
 
         case 1: ;               //fan
-            ir_out2[9]=0x27;
-            ir_out2[10]=0xD8;
+            ir_out2[10]=0x27;
+            ir_out2[11]=0xD8;
             p_m=1;
             break;
         case 2: ;               //cool
-            ir_out2[9]|=0x06;
-            ir_out2[10]|=0x09;
+            ir_out2[10]|=0x06;
+            ir_out2[11]|=0x09;
             p_m=2;
             break;
         case 3: ;               //heat
-            ir_out2[9]|=0x03;
-            ir_out2[10]|=0x0C;
+            ir_out2[10]|=0x03;
+            ir_out2[11]|=0x0C;
             p_m=3;
             break;
         case 4: ;               //dry
-            ir_out2[9]|=0x05;
-            ir_out2[10]|=0x0A;
+            ir_out2[10]|=0x05;
+            ir_out2[11]|=0x0A;
             p_m=4;
             break;
         case 5: ;               //Off. Depends on the previous mode
             switch(p_m) {
                 case 1: ;
-                    ir_out2[9]|=0x0F;
-                    ir_out2[10]|=0x00;
+                    ir_out2[10]|=0x0F;
+                    ir_out2[11]|=0x00;
                     break;
                 case 2: ;
-                    ir_out2[9]|=0x0E;
-                    ir_out2[10]|=0x01;
+                    ir_out2[10]|=0x0E;
+                    ir_out2[11]|=0x01;
                     break;
                 case 3: ;
-                    ir_out2[9]|=0x0B;
+                    ir_out2[10]|=0x0B;
                     ir_out2[11]|=0x04;
                     break;
                 case 4: ;
-                    ir_out2[9]|=0x0D;
+                    ir_out2[10]|=0x0D;
                     ir_out2[11]|=0x02;
                     break;
 
@@ -142,17 +142,19 @@ function ir_generator(irInfo)
     //Mode: Fan=1, Cool=2, Heat=3, Humidity=4, Off=5
     //Fan mode: Auto=1, High=2, Med=3, Low=4
 
-    ir_out2[0]=0x52;
-    ir_out2[1]=0xAE;
-    ir_out2[2]=0xC3;
-    ir_out2[3]=0x26;
-    ir_out2[4]=0xD9;
-    
+    ir_out2[1]=0x52;
+    ir_out2[2]=0xAE;
+    ir_out2[3]=0xC3;
+    ir_out2[4]=0x26;
+    ir_out2[5]=0xD9;
+
     settemp(irInfo.temp);
     setfan(irInfo.fan);
     setswing(irInfo.hsw, irInfo.vsw);
     setmode(irInfo.mode);
     lsb8_to_msb();   
+	
+	ir_out2[0]=0x58; // Length to consider
     
     console.log(ir_out2);
 
@@ -168,9 +170,9 @@ function ir_generator(irInfo)
 
 var xyz = {};
 xyz.temp = 23;
-xyz.fan = 2;
+xyz.fan = 1;
 xyz.hsw = 0;
-xyz.vsw = 1;
+xyz.vsw = 0;
 xyz.mode = 2;
 
 ir_generator(xyz);
